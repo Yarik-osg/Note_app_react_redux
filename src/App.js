8e5a3ba4534045b006ff131f8c173bf824f2c2cc
Yaroslav <yaroslav.brila@gmail.com>
2021-10-13 23:20:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {ToastContainer} from "react-toastify";
+import Navbar from "./components/Navbar";
+import {Route} from "react-router-dom";
+import Home from "./components/Home";
+import AddNote from "./components/AddNote";
+import EditNote from "./components/EditNote";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <ToastContainer />
+            <Navbar />
+            <Route exact path="/" component={() => <Home />} />
+            <Route exact path="/add" component={() => <AddNote />} />
+            <Route exact path="/edit/:id" component={() => <EditNote />} />
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "react-toastify/dist/ReactToastify.css"
+import {BrowserRouter as Router} from "react-router-dom";
+import {createStore} from "redux";
+import {noteReducer} from "./Redux/noteReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {Provider} from "react-redux";
 
+const store = createStore(noteReducer, composeWithDevTools())
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
